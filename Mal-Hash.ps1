b8f2c412d7f325671edca1ad3c808f3ae1940506
Doug Metz <dwmetz@gmail.com>
2023-07-13 09:38:28 -0400

    MB API
    Scripts now use the calculated SHA256 to verify whether the sample is present on Malware Bazaar

--- Mal-Hash.ps1
+++ Mal-Hash.ps1
@@ -16,6 +16,7 @@ Prerequisites:
                 UTC timestamp in report
                 report name change
 5-July-2023 Changed default hash value submission to SHA256
+13-July $report updates
 #>
 Clear-Host
 Write-Host ""
@@ -36,7 +37,7 @@ Write-host -Fore DarkCyan "
        .';:clooddddolc:,..                                  
            ''''''''''                                                                                                                 
 "                
-Write-Host -Fore DarkCyan "                    Mal-Hash v1.5" 
+Write-Host -Fore DarkCyan "                    Mal-Hash v1.6" 
 Write-Host -Fore Gray "          https://github.com/dwmetz/Mal-Hash"
 Write-Host -Fore Gray "          @dwmetz | bakerstreetforensics.com"
 Write-Host ""
@@ -46,29 +47,23 @@ $tstamp = (Get-Date -Format "yyyyMMddHHmm")
 $script:file = Read-Host -Prompt 'enter path and filename'
 write-host " "
 $sourcefile = [system.IO.Path]::GetFileName("$script:file")
-"SOURCE: $sourcefile" | Out-File -FilePath malhash.-t.txt -Append
-" " | Out-File -FilePath malhash.-t.txt -Append
+$report = "malhash" + "-" + $sourcefile + "-" + $tstamp + ".txt" 
+"SOURCE: $sourcefile" | Out-File -FilePath $report -Append
+" " | Out-File -FilePath $report -Append
 $datetime = Get-Date
 $date = $datetime.ToUniversalTime()
-"DATE/TIME UTC: $date" | Out-File -FilePath malhash.-t.txt -Append
-" " | Out-File -FilePath malhash.-t.txt -Append
+"DATE/TIME UTC: $date" | Out-File -FilePath $report -Append
+" " | Out-File -FilePath $report -Append
 $apiKey = (Get-Content vt-api.txt)
 $MD5hash = (Get-FileHash $file -Algorithm MD5).Hash 
 $SHA1hash = (Get-FileHash $file -Algorithm SHA1).Hash
 $SHA256hash = (Get-FileHash $file -Algorithm SHA256).Hash
-"** HASHES: **" | Out-File -FilePath malhash.-t.txt -Append
-"MD5: $MD5hash" | Out-File -FilePath malhash.-t.txt -Append
-"SHA1: $SHA1hash" | Out-File -FilePath malhash.-t.txt -Append
-"SHA256: $SHA256hash" | Out-File -FilePath malhash.-t.txt -Append
-" " | Out-File -FilePath malhash.-t.txt -Append
-"** STRINGS: ** " | Out-File -FilePath malhash.-t.txt -Append
-strings -n 8 $script:file | Out-File -FilePath malhash.-t.txt -Append
-write-host "STRINGS:" -Fore Cyan
-strings -n 8 $script:file
-" " | Out-File -FilePath malhash.-t.txt -Append
-Write-host ""
-# Comment out below to skip VT query (offline analysis)
-"** VIRUS TOTAL RESULTS: **" | Out-File -FilePath malhash.-t.txt -Append
+"** HASHES: **" | Out-File -FilePath $report -Append
+"MD5: $MD5hash" | Out-File -FilePath $report -Append
+"SHA1: $SHA1hash" | Out-File -FilePath $report -Append
+"SHA256: $SHA256hash" | Out-File -FilePath $report -Append
+" " | Out-File -FilePath $report -Append
+"** VIRUS TOTAL RESULTS: **" | Out-File -FilePath $report -Append
 $fileHash = (Get-FileHash $file -Algorithm SHA256).Hash
 write-host "Submitting SHA256 hash $fileHash to Virus Total" -Fore Cyan
 Write-host ""
@@ -76,19 +71,36 @@ $uri = "https://www.virustotal.com/vtapi/v2/file/report?apikey=$apiKey&resource=
 write-host "VIRUS TOTAL RESULTS:" -Fore Cyan
 Invoke-RestMethod -Uri $uri
 $vtResults = Invoke-RestMethod -Uri $uri
-Invoke-RestMethod -Uri $uri | Out-File -FilePath malhash.-t.txt -Append
+Invoke-RestMethod -Uri $uri | Out-File -FilePath $report -Append
 $vtresults
 $vtResults.scans 
-$vtResults.scans | Out-File -FilePath malhash.-t.txt -Append
-#>
-" " | Out-File -FilePath malhash.-t.txt -Append
-"** END REPORT **" | Out-File -FilePath malhash.-t.txt -Append
-$report = $sourcefile + "." + $tstamp
-Get-ChildItem -Filter 'malhash*' -Recurse | Rename-Item -NewName {$_.name -replace '-t', $report }
+$vtResults.scans | Out-File -FilePath $report -Append
+Write-host " "
+"** STRINGS: ** " | Out-File -FilePath $report -Append
+strings -n 8 $script:file | Out-File -FilePath $report -Append
+write-host "STRINGS:" -Fore Cyan
+strings -n 8 $script:file
+" " | Out-File -FilePath $report -Append
 Write-host " "
 Write-host $vtResults.positives of $vtResults.total vendors detected this sample.
 Write-host " "
-Write-host -Fore Green "VT Results Permalink:"
-Write-host $vtResults.Permalink
-Write-host " "
-Write-host "Mal-Hash complete. Report saved as malhash.$report.txt" -Fore Cyan
+Write-host -Fore Green "VT Results Permalink:" | Out-File -FilePath $report -Append
+Write-host $vtResults.Permalink | Out-File -FilePath $report -Append
+Write-host " " | Out-File -FilePath $report -Append
+" " | Out-File -FilePath $report -Append
+#
+$report = "malhash" + "-" + $sourcefile + "-" + $tstamp + ".txt" 
+"** MALWARE BAZAAR RESULTS: **" | Out-File -FilePath $report -Append
+" " | Out-File -FilePath $report -Append
+Write-host -Fore Green "Malware Bazaar Results:
+" 
+$url = "https://mb-api.abuse.ch/api/v1/"
+$data = @{
+    query = "get_info"
+    hash = $SHA256hash
+}
+$mb = Invoke-RestMethod -Uri $url -Method POST -Body $data
+$mb.data
+$mb.data | Out-File -FilePath $report -Append
+"** END REPORT **" | Out-File -FilePath $report -Append
+Write-host "Mal-Hash complete. Report saved as $report" -Fore Cyan
--- VTHashSub.ps1
+++ VTHashSub.ps1
@@ -20,37 +20,54 @@ Write-host -Fore Cyan "
        .';:clooddddolc:,..                                  
            ''''''''''                                                                                                                 
 "                
-Write-Host -Fore Cyan "               VTHashSub v1.0" 
+Write-Host -Fore Cyan "               VTHashSub v1.2" 
 Write-Host -Fore Gray "     @dwmetz | bakerstreetforensics.com"
 Write-Host ""
 Write-Host ""
 Write-Host -Fore DarkCyan "   It submits the hash to VirusTotal or it"
 Write-Host -Fore DarkCyan "   gets the hose again."
 write-host " "
-$tstamp = (Get-Date -Format "yyyy-MM-dd-HH-mm")
+$tstamp = (Get-Date -Format "yyyyMMddHHmm")
 $script:hash = Read-Host -Prompt 'enter the malware hash value'
+$report = "malhash" + "-" + $script:hash + "-" + $tstamp + ".txt" 
 write-host " "
-"HASH: $script:hash" | Out-File -FilePath malhash.-t.txt -Append
-" " | Out-File -FilePath malhash.-t.txt -Append
+"HASH: $script:hash" | Out-File -FilePath $report -Append
+" " | Out-File -FilePath $report -Append
 $datetime = Get-Date
 $date = $datetime.ToUniversalTime()
-"DATE/TIME UTC: $date" | Out-File -FilePath malhash.-t.txt -Append
-" " | Out-File -FilePath malhash.-t.txt -Append
+"DATE/TIME UTC: $date" | Out-File -FilePath $report -Append
+" " | Out-File -FilePath $report -Append
 $apiKey = (Get-Content vt-api.txt)
-"** VIRUS TOTAL RESULTS: **" | Out-File -FilePath malhash.-t.txt -Append
+"** VIRUS TOTAL RESULTS: **" | Out-File -FilePath $report -Append
 write-host "Submitting the hash $script:hash to Virus Total" -Fore DarkCyan
 Write-host ""
 $uri = "https://www.virustotal.com/vtapi/v2/file/report?apikey=$apiKey&resource=$script:hash"
 write-host "VIRUS TOTAL RESULTS:" -Fore Cyan
 Invoke-RestMethod -Uri $uri
 $vtResults = Invoke-RestMethod -Uri $uri
-Invoke-RestMethod -Uri $uri | Out-File -FilePath malhash.-t.txt -Append
+Invoke-RestMethod -Uri $uri | Out-File -FilePath $report -Append
 $vtresults
 $vtResults.scans 
-$vtResults.scans | Out-File -FilePath malhash.-t.txt -Append
-" " | Out-File -FilePath malhash.-t.txt -Append
-"** END REPORT **" | Out-File -FilePath malhash.-t.txt -Append
-$report = $script:hash + "." + $tstamp
-Get-ChildItem -Filter 'malhash*' -Recurse | Rename-Item -NewName {$_.name -replace '-t', $report }
+$vtResults.scans | Out-File -FilePath $report -Append
+" " | Out-File -FilePath $report -Append
 Write-host " "
-Write-host "VTHashSub complete. Report saved as malhash.$report.txt" -Fore Cyan
+Write-host $vtResults.positives of $vtResults.total vendors detected this sample.
+Write-host " "
+Write-host -Fore Green "VT Results Permalink:"
+Write-host $vtResults.Permalink
+Write-host " "
+#
+$report = "malhash" + "-" + $script:hash + "-" + $tstamp + ".txt" 
+"** MALWARE BAZAAR RESULTS: **" | Out-File -FilePath $report -Append
+Write-host -Fore Green "Malware Bazaar Results:
+" 
+$url = "https://mb-api.abuse.ch/api/v1/"
+$data = @{
+    query = "get_info"
+    hash = $script:hash
+}
+$mb = Invoke-RestMethod -Uri $url -Method POST -Body $data
+$mb.data
+$mb.data | Out-File -FilePath $report -Append
+"** END REPORT **" | Out-File -FilePath $report -Append
+Write-host "VTHashSub complete. Report saved as $report" -Fore Cyan
